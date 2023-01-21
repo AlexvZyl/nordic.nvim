f3778ec71c01b23317dad156679b2cd10e2dd722
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-21 22:53:08 +0200

    Add use cases to palette

--- lua/nordic/colors.lua
+++ lua/nordic/colors.lua
@@ -5,6 +5,7 @@ local palette = {
     -- Darker colors.
     -- Not in base Nord.
     black = "#191C24",
+    -- This color is actually used on their website's dark theme.
     gray0 = "#242933",
 
     -- Polar Night.
@@ -14,11 +15,11 @@ local palette = {
     gray4 = "#4C566A",
 
     -- A light blue/gray (nice for comments).
-    -- @nightfox.nvim.
+    -- From @nightfox.nvim.
     gray5 = "#60728A",
 
     -- Dim white.
-    -- @nightfox.nvim.
+    -- From @nightfox.nvim.
     white0 = "#BBC3D4",
 
     -- Snow storm.
@@ -40,7 +41,7 @@ local palette = {
     -- Aurora.
     -- These colors are used a lot, so we need variations for them.
     -- Base colors are from original Nord palette.
-    -- Bright & dim @nightfox.nvim.
+    -- Bright & dim from @nightfox.nvim.
     red = {
         base = "#BF616A",
         bright = "#D06F79",
@@ -65,18 +66,52 @@ local palette = {
         base = "#B48EAD",
         bright = "#C895BF",
         dim = "#9D7495",
+    },
+    -- From @nightfox.nvim.
+    blue = {
+        base = "#81A1C1",
+        bright = "8CAFD2",
+        dim = "#668AAB",
     }
 
 }
 
 -- Now define some use cases.
+-- Some of the format is from @folke/tokyonight.nvim.
+
+-- Backgrounds.
 palette.bg = palette.gray0
-palette.bg_alt = palette.black
+palette.bg_highlight = palette.black
+palette.bg_dark = palette.black
+palette.bg_sidebar = palette.bg
+palette.bg_float = palette.bg
+
+-- Foregrounds.
 palette.fg = palette.white0
-palette.fg_alt = palette.white1
-palette.comment = palette.gray5
-palette.border = palette.white1
-palette.border_alt = palette.gray4
-palette.border_nb = palette.cyan
+palette.fg_dark = palette.white0
+palette.fg_sidebar = palette.fg
+palette.fg_gutter = palette.gray2
+palette.fg_float = palette.white1
+
+-- Borders.
+palette.border = palette.black
+palette.border_float = palette.white1
+palette.border_nb = palette.orange.base
+
+-- Diffs.
+palette.diff = {}
+palette.diff.add = palette.green.bright
+palette.diff.change = palette.blue.bright
+palette.diff.text = palette.fg
+palette.diff.delete = palette.red.bright
+
+-- Diagnostics.
+palette.error = palette.red.bright
+palette.warn = palette.yellow.base
+palette.hint = palette.green.bright
+palette.info = palette.blue.bright
+
+-- Misc.
+palette.comment = palette.gray4
 
 return palette
