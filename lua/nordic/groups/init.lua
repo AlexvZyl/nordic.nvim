541e48a9070bfdd261c776e18f6d3ab3ba9df9b7
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-21 21:51:53 +0200

    Highlighting utilities and groups

--- lua/nordic/groups/init.lua
+++ lua/nordic/groups/init.lua
@@ -1,7 +1,40 @@
+local merge = require 'nordic.utils'.merge
+
 local M = {}
 
+M.integrations = {
+    "barbar",
+    "dap-ui",
+    "dap",
+    "dashboard",
+    "indent-blankline",
+    "lspsaga",
+    "lualine",
+    "nvim-tree",
+    "treesitter"
+}
+
+M.core = {
+    "editor",
+    "syntax",
+    "terminal"
+}
+
 function M.get_groups()
-    return {}
+    local groups = {}
+    for integration in M.integrations do
+        groups = merge(
+            groups,
+            require("nordic.groups.integrations." .. integration)
+        )
+    end
+    for core in M.core do
+        groups = merge(
+            groups,
+            require("nordic.groups." .. core)
+        )
+    end
+    return groups
 end
 
 return M
--- lua/nordic/utils.lua
+++ lua/nordic/utils.lua
@@ -7,4 +7,13 @@ function M.highlight(table)
     end
 end
 
+-- Merge two lua tables.
+function M.merge(table1, table2)
+    return vim.tbl_deep_extend(
+        "force",
+        table1,
+        table2
+    )
+end
+
 return M
