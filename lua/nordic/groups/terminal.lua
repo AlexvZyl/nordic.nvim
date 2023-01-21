f7edccdfa44f2ac71eacac5f4346ef957e9780ef
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-21 23:51:19 +0200

    Add terminal colors

--- lua/nordic/groups/terminal.lua
+++ lua/nordic/groups/terminal.lua
@@ -1,3 +1,27 @@
-return {
+local c = require 'nordic.colors'
 
+return {
+        terminal_color_0 = c.black,
+		terminal_color_8 = c.gray0,
+
+		terminal_color_1 = c.red.base,
+		terminal_color_9 = c.red.bright,
+
+		terminal_color_2 = c.green.base,
+		terminal_color_10 = c.green.bright,
+
+		terminal_color_3 = c.yellow.base,
+		terminal_color_11 = c.yellow.bright,
+
+		terminal_color_4 = c.blue0,
+		terminal_color_12 = c.blue2,
+
+		terminal_color_5 = c.magenta.base,
+		terminal_color_13 = c.magenta.bright,
+
+		terminal_color_6 = c.cyan.base,
+		terminal_color_14 = c.cyan.bright,
+
+		terminal_color_7 = c.white0,
+		terminal_color_15 = c.white1,
 }
