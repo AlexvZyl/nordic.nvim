18fb36d6380dcbf0cdfd70283aa507625e57b550
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-22 00:39:53 +0200

    Add syntax and treesitter support

--- lua/nordic/groups/integrations/treesitter.lua
+++ lua/nordic/groups/integrations/treesitter.lua
@@ -1,3 +1,120 @@
-return {
+-- Notes and format from @folke/tokyonight.nvim.
 
+local c = require 'nordic.colors'
+
+return {
+    -- These groups are for the neovim tree-sitter highlights.
+    -- As of writing, tree-sitter support is a WIP, group names may change.
+    -- By default, most of these groups link to an appropriate Vim group,
+    -- TSError -> Error for example, so you do not have to define these unless
+    -- you explicitly want to support Treesitter's improved syntax awareness.
+
+    -- TSAnnotation        = { };    -- For C++/Dart attributes, annotations that can be attached to the code to denote some kind of meta information.
+    -- TSAttribute         = { };    -- (unstable) TODO: docs
+    -- TSBoolean           = { };    -- For booleans.
+    -- TSCharacter         = { };    -- For characters.
+    -- TSComment           = { };    -- For comment blocks.
+    TSNote = { fg = c.bg, bg = c.info },
+    ["@text.warning"] = { fg = c.bg, bg = c.warning },
+    ["@text.danger"] = { fg = c.bg, bg = c.error },
+    ["@constructor"] = { fg = c.magenta.base }, -- For constructor calls and definitions: `= { }` in Lua, and Java constructors.
+    -- TSConditional       = { };    -- For keywords related to conditionnals.
+    -- TSConstant          = { };    -- For constants
+    -- TSConstBuiltin      = { };    -- For constant that are built in the language: `nil` in Lua.
+    -- TSConstMacro        = { };    -- For constants that are defined by macros: `NULL` in C.
+    -- TSError             = { };    -- For syntax/parser errors.
+    -- TSException         = { };    -- For exception related keywords.
+    ["@field"] = { fg = c.green.base }, -- For fields.
+    -- TSFloat             = { };    -- For floats.
+    -- TSFunction          = { };    -- For function (calls and definitions).
+    -- TSFuncBuiltin       = { };    -- For builtin functions: `table.insert` in Lua.
+    -- TSFuncMacro         = { };    -- For macro defined fuctions (calls and definitions): each `macro_rules` in Rust.
+    -- TSInclude           = { };    -- For includes: `#include` in C, `use` or `extern crate` in Rust, or `require` in Lua.
+    ["@keyword"] = { fg = c.orange.base }, -- For keywords that don't fall in previous categories.
+    ["@keyword.function"] = { fg = c.orange.base }, -- For keywords used to define a fuction.
+    ["@label"] = { fg = c.blue1 }, -- For labels: `label:` in C and `:label:` in Lua.
+    -- TSMethod            = { };    -- For method calls and definitions.
+    -- TSNamespace         = { };    -- For identifiers referring to modules and namespaces.
+    -- TSNone              = { };    -- TODO: docs
+    -- TSNumber            = { };    -- For all numbers
+    ["@operator"] = { fg = c.orange.bright }, -- For any operator: `+`, but also `->` and `*` in C.
+    ["@parameter"] = { fg = c.white0 }, -- For parameters of a function.
+    -- TSParameterReference= { };    -- For references to parameters of a function.
+    ["@property"] = { fg = c.cyan.base }, -- Same as `TSField`.
+    ["@punctuation.delimiter"] = { fg = c.orange.bright }, -- For delimiters ie: `.`
+    ["@punctuation.bracket"] = { fg = c.white0}, -- For brackets and parens.
+    ["@punctuation.special"] = { fg = c.orange.bright }, -- For special punctutation that does not fall in the catagories before.
+    -- TSRepeat            = { };    -- For keywords related to loops.
+    -- TSString            = { };    -- For strings.
+    ["@string.regex"] = { fg = c.green.dim }, -- For regexes.
+    ["@string.escape"] = { fg = c.green.bright }, -- For escape characters within a string.
+    -- TSSymbol            = { };    -- For identifiers referring to symbols or atoms.
+    -- TSType              = { };    -- For types.
+    -- TSTypeBuiltin       = { };    -- For builtin types.
+    ["@variable"] = { fg = c.white0 }, -- Any variable name that does not have another highlight.
+    ["@variable.builtin"] = { fg = c.blue1 }, -- Variable names that are defined by the languages, like `this` or `self`.
+
+    -- TSTag               = { };    -- Tags like html tag names.
+    -- TSTagDelimiter      = { };    -- Tag delimiter like `<` `>` `/`
+    -- TSText              = { };    -- For strings considered text in a markup language.
+    ["@text.reference"] = { fg = c.green.base },
+    -- TSEmphasis          = { };    -- For text to be represented with emphasis.
+    -- TSUnderline         = { };    -- For text to be represented with an underline.
+    -- TSStrike            = { };    -- For strikethrough text.
+    -- TSTitle             = { };    -- Text that is part of a title.
+    -- TSLiteral           = { };    -- Literal text.
+    -- TSURI               = { };    -- Any URI like a link or email.
+    ["@text.diff.add"] = { link = "DiffAdd" },
+    ["@text.diff.delete"] = { link = "DiffDelete" },
+
+    -- Lua
+    -- luaTSProperty = { fg = c.red }, -- Same as `TSField`.
+
+    -- FROM LEGACY CODEBASE.
+    -----------------------
+    -- Magentas.
+    ["@constant"] = { fg = c.magenta.bright },
+    ["Number"] = { fg = c.magenta.bright },
+    ["@number"] = { fg = c.magenta.bright },
+    ["@float"] = { fg = c.magenta.bright },
+    ["@boolean"] = { fg = c.magenta.bright },
+    ["@constant.macro"] = { fg = c.magenta.bright },
+    -- Oranges (Keywords).
+    -- ["@keyword"] = { fg = c.orange, style = { 'bold' } },
+    ["@keyword.return"] = { fg = c.orange.base, bold = true },
+    -- ["@keyword.function"] = { fg = c.orange.base, bold = true },
+    ["@keyword.export"] = { fg = c.orange.base, bold = true },
+    ["@keyword.operator"] = { fg = c.orange.base, bold = true },
+    ["@repeat"] = { fg = c.orange.base, bold = true },
+    ["@conditional"] = { fg = c.orange.base, bold = true },
+    -- ["@operator"] = { fg = c.orange.bright, bold = true },
+    ["@class"] = { fg = c.orange.base, bold = true },
+    ["@type.qualifier"] = { fg = c.orange.base, bold = true },
+    ["@storageclass"] = { fg = c.orange.base, bold = true },
+    -- Reds.
+    ["@preproc"] = { fg = c.red.base, bold = true },
+    ["@attribute"] = { fg = c.red.base, bold = true },
+    ["@include"] = { fg = c.red.base, bold = true },
+    ["@macro"] = { fg = c.red.base, bold = true },
+    ["@function.macro"] = { fg = c.red.base, bold = true },
+    ["@define"] = { fg = c.red.base, bold = true },
+    ["@exception"] = { fg = c.red.base, bold = true },
+    -- ["@punctuation.special"] = { fg = c.red, style = { 'bold' } },
+    -- Blues.
+    ["@function"] = { fg = c.blue2 },
+    ["@method"] = { fg = c.blue2 },
+    ["@method.call"] = { fg = c.blue2 },
+    ["@function.call"] = { fg = c.blue2 },
+    ["@function.builtin"] = { fg = c.blue2 },
+    -- ["@variable.builtin"] = { fg = c.blue0, italic = true },
+    -- Cyan.
+    -- ["@field"] = { fg = c.cyan.base },
+    -- ["@property"] = { fg = c.cyan.base },
+    ["@property.cpp"] = { fg = c.cyan.base },
+    -- Yellows.
+    ["@namespace"] = { fg = c.yellow.dim, italic = true },
+    ["@type"] = { fg = c.yellow.base },
+    ["@type.builtin"] = { fg = c.yellow.base },
+    ["@type.definition"] = { fg = c.yellow.base },
+    -----------------------
 }
--- lua/nordic/groups/syntax.lua
+++ lua/nordic/groups/syntax.lua
@@ -1,3 +1,115 @@
+-- Notes and format from @folke/tokyonight.nvim.
+
+local c = require 'nordic.colors'
+
 return {
 
+    Constant    = { fg = c.magenta.bright }, -- (preferred) any constant
+    String      = { fg = c.green.base }, --   a string constant: "this is a string"
+    Character   = { fg = c.green.bright }, --  a character constant: 'c', '\n'
+    Number      = { fg = c.magenta.bright  }, --   a number constant: 234, 0xff
+    Boolean     = { fg = c.magenta.bright  }, --  a boolean constant: TRUE, false
+    Float       = { fg = c.magenta.bright  }, --    a floating point constant: 2.3e10
+
+    Identifier    = { fg = c.fg }, -- (preferred) any variable name
+    Function      = { fg = c.orange.base }, -- function name (also: methods for classes)
+
+    Statement     = { fg = c.orange.base }, -- (preferred) any statement
+    Conditional   = { fg = c.orange.base  }, --  if, then, else, endif, switch, etc.
+    Repeat        = { fg = c.orange.base  }, --   for, do, while, etc.
+    Label         = { fg = c.orange.base  }, --    case, default, etc.
+    Operator      = { fg = c.orange.bright }, -- "sizeof", "+", "*", etc.
+    Keyword       = { fg = c.orange.base }, --  any other keyword
+    Exception     = { fg = c.orange.bright  }, --  try, catch, throw
+
+    PreProc       = { fg = c.red.base, bold = true }, -- (preferred) generic Preprocessor
+    Include       = { fg = c.red.base, bold = true }, --  preprocessor #include
+    Define        = { fg = c.red.base, bold = true }, --   preprocessor #define
+    Macro         = { fg = c.red.base, bold = true }, --    same as Define
+    PreCondit     = { fg = c.red.base, bold = true }, --  preprocessor #if, #else, #endif, etc.
+
+    Type = { fg = c.yellow.base }, -- (preferred) int, long, char, etc.
+    StorageClass  = { fg = c.orange.base }, -- static, register, volatile, etc.
+    Structure     = { fg = c.orange.base }, --  struct, union, enum, etc.
+    Typedef       = { fg = c.yellow.base }, --  A typedef
+
+    -- What are these? TODO.
+    Special = { fg = c.blue1 }, -- (preferred) any special symbol
+    -- SpecialChar   = { }, --  special character in a constant
+    -- Tag           = { }, --    you can use CTRL-] on this
+    -- Delimiter     = { }, --  character that needs attention
+    -- SpecialComment= { }, -- special things inside a comment
+    -- Debug         = { }, --    debugging statements
+
+    Underlined = { underline = true }, -- (preferred) text that stands out, HTML links
+    Bold = { bold = true },
+    Italic = { italic = true },
+
+    -- ("Ignore", below, may be invisible...)
+    -- Ignore = { }, -- (preferred) left blank, hidden  |hl-Ignore|
+
+    Error = { fg = c.error }, -- (preferred) any erroneous construct
+    Todo = { bg = c.yellow.dim, fg = c.bg }, -- (preferred) anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+
+    -- What are these? TODO.
+    qfLineNr = { fg = c.gray4 },
+    qfFileName = { fg = c.blue1 },
+
+    -- What are these? TODO.
+    htmlH1 = { fg = c.magenta, bold = true },
+    htmlH2 = { fg = c.blue1, bold = true },
+
+    -- mkdHeading = { fg = c.orange, bold = true },
+    -- mkdCode = { bg = c.terminal_black, fg = c.fg },
+    mkdCodeDelimiter = { bg = c.black, fg = c.fg },
+    mkdCodeStart = { fg = c.cyan.base, bold = true },
+    mkdCodeEnd = { fg = c.cyan.base, bold = true },
+    -- mkdLink = { fg = c.blue, underline = true },
+
+    markdownHeadingDelimiter = { fg = c.orange, bold = true },
+    markdownCode = { fg = c.cyan.base },
+    markdownCodeBlock = { fg = c.cyan.base },
+    markdownH1 = { fg = c.magenta.base, bold = true },
+    markdownH2 = { fg = c.blue1, bold = true },
+    markdownLinkText = { fg = c.blue1, underline = true },
+
+    ["@punctuation.special.markdown"] = { fg = c.orange.base, bold = true },
+    ["@text.todo.unchecked"] = { fg = c.blue1 }, -- For brackets and parens.
+    ["@text.todo.checked"] = { fg = c.green.base }, -- For brackets and parens.
+    ["@text.literal.markdown_inline"] = { bg = c.black, fg = c.blue1 },
+    ["@text.literal.markdown"] = { link = "Normal" },
+    ["helpCommand"] = { bg = c.black, fg = c.blue },
+
+    debugPC = { bg = c.bg_sidebar }, -- used for highlighting the current line in terminal-debug
+    debugBreakpoint = { bg = c.black, fg = c.info }, -- used for breakpoint colors in terminal-debug
+
+    -- These groups are for the native LSP client. Some other LSP clients may
+    -- use these groups, or use their own. Consult your LSP client's
+    -- documentation.
+    LspReferenceText = { bg = c.fg_gutter }, -- used for highlighting "text" references
+    LspReferenceRead = { bg = c.fg_gutter }, -- used for highlighting "read" references
+    LspReferenceWrite = { bg = c.fg_gutter }, -- used for highlighting "write" references
+
+    DiagnosticError = { fg = c.error }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default
+    DiagnosticWarn = { fg = c.warning }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default
+    DiagnosticInfo = { fg = c.info }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default
+    DiagnosticHint = { fg = c.hint }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default
+
+    DiagnosticVirtualTextError = { bg = c.gray3, fg = c.error }, -- Used for "Error" diagnostic virtual text
+    DiagnosticVirtualTextWarn = { bg = c.gray3, fg = c.warning }, -- Used for "Warning" diagnostic virtual text
+    DiagnosticVirtualTextInfo = { bg = c.gray3, fg = c.info }, -- Used for "Information" diagnostic virtual text
+    DiagnosticVirtualTextHint = { bg = c.gray3, fg = c.hint }, -- Used for "Hint" diagnostic virtual text
+
+    DiagnosticUnderlineError = { undercurl = true, sp = c.error }, -- Used to underline "Error" diagnostics
+    DiagnosticUnderlineWarn = { undercurl = true, sp = c.warning }, -- Used to underline "Warning" diagnostics
+    DiagnosticUnderlineInfo = { undercurl = true, sp = c.info }, -- Used to underline "Information" diagnostics
+    DiagnosticUnderlineHint = { undercurl = true, sp = c.hint }, -- Used to underline "Hint" diagnostics
+
+    LspSignatureActiveParameter = { bg = c.gray3, bold = true },
+    LspCodeLens = { fg = c.comment },
+
+    LspInfoBorder = { fg = c.border, bg = c.bg_float },
+
+    ALEErrorSign = { fg = c.error },
+    ALEWarningSign = { fg = c.warning },
 }
