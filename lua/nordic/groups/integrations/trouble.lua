7ddd9b07b282a339868d460d7ef4336f7425046a
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-22 14:58:37 +0200

    Trouble support

--- lua/nordic/groups/integrations/trouble.lua
+++ lua/nordic/groups/integrations/trouble.lua
@@ -1,3 +1,18 @@
+local c = require 'nordic.colors'
+
 return {
+
+    TroubleNormal = {
+        bg = c.black
+    },
+
+    TroubleText = {
+        fg = c.fg
+    },
+
+    TroubleCount = {
+        fg = c.white1,
+        bg = c.gray2
+    }
 
 }
