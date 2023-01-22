ce31d87d669498d59e8910e49a120a74a99dedfb
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-22 14:42:01 +0200

    Initial lspsaga support

--- lua/nordic/groups/integrations/lspsaga.lua
+++ lua/nordic/groups/integrations/lspsaga.lua
@@ -1,3 +1,18 @@
+local c = require 'nordic.colors'
+
 return {
+
+    -- LSP Saga.
+    ["SagaBorder"] = {
+        fg = c.white0
+    },
+
+    ["DefinitionBorder"] = {
+        fg = c.white0
+    },
+
+    ["DefinitionNormal"] = {
+        fg = c.gray0
+    },
 
 }
