69c545948a1c5eb5a5f71f7b898211d83812638e
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-22 15:02:36 +0200

    Dap-ui support

--- lua/nordic/groups/integrations/dap-ui.lua
+++ lua/nordic/groups/integrations/dap-ui.lua
@@ -1,3 +1,65 @@
+local c = require 'nordic.colors'
+
 return {
+
+    DapUIStop = {
+        fg = c.red.bright,
+        bold = true
+    },
+
+    DapUIRestart = {
+        fg = c.green.bright,
+        bold = true
+    },
+
+    DapUIPlayPause = {
+        fg = c.green.bright,
+        bold = true,
+    },
+
+    DapUIStepInto = {
+        fg = c.blue2,
+        bold = true
+    },
+
+    DapUIStepOver = {
+        fg = c.blue2,
+        bold = true,
+    },
+
+    DapUIStepOut = {
+        fg = c.blue2,
+        bold = true
+    },
+
+    DapUIStepBack = {
+        fg = c.blue2,
+        bold = true
+    },
+
+    DapUIValue = {
+        fg = c.magenta.bright
+    },
+
+    DapUIWatchesValue = {
+        fg = c.green.bright
+    },
+
+    DapUIType = {
+        fg = c.yellow
+    },
+
+    DapUIFloatBorder = {
+        fg = c.orange.base,
+        bg = c.black
+    },
+
+    DapUIFloatNormal = {
+        bg = c.gray0
+    },
+
+    DapUILineNumber = {
+        fg = c.green.bright
+    },
 
 }
