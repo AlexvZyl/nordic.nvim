61f49b9f75d8a37e6a7c16c7fc6ea3e5b5e055ce
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-22 14:40:56 +0200

    Dashboard support

--- lua/nordic/groups/integrations/dashboard.lua
+++ lua/nordic/groups/integrations/dashboard.lua
@@ -1,3 +1,18 @@
+local c = require 'nordic.colors'
+
 return {
+
+    DashboardHeader = {
+        fg = c.yellow.base,
+        bold = true
+    },
+
+    DashboardFooter = {
+        fg = c.cyan.base,
+        bold = true,
+    },
+    DashboardCenter = {
+        fg = c.green.base
+    },
 
 }
