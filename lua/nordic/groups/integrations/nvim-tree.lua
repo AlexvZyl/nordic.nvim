c75242d551b6f99e70b29d1586313655d7af6ae4
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-22 02:01:09 +0200

    Add nvimtree support

--- lua/nordic/groups/integrations/nvim-tree.lua
+++ lua/nordic/groups/integrations/nvim-tree.lua
@@ -1,3 +1,26 @@
+local c = require 'nordic.colors'
+
 return {
+
+    NvimTreeFolderIcon = { fg = c.yellow.dim },
+    NvimTreeFolderName = { fg = c.blue1 },
+    NvimTreeEmptyFolderName = { fg = c.blue1 },
+    NvimTreeOpenedFolderName = { fg = c.blue2 },
+    NvimTreeOpenedName = { fg = c.blue1 },
+    NvimTreeRootFolder = { fg = c.yellow.base, bold = true },
+    NvimTreeNormal = { fg = c.white0, bg = c.black },
+    NvimTreeNormalNC = { fg = c.white0, bg = c.black },
+    NvimTreeIndentMarker = { fg = c.gray3 },
+
+    NvimTreeWinSeparator = { fg = c.bg_sidebar, bg = c.bg_sidebar },
+    NvimTreeGitDirty = { fg = c.git.change },
+    NvimTreeGitNew = { fg = c.git.add },
+    NvimTreeGitDeleted = { fg = c.git.delete },
+    NvimTreeOpenedFile = { bg = c.black },
+    NvimTreeSpecialFile = { fg = c.magenta.brigt, underline = true },
+    NvimTreeImageFile = { fg = c.fg_sidebar },
+    NvimTreeSymlink = { fg = c.blue1 },
+    NeoTreeNormal = { fg = c.white0, bg = c.black },
+    NeoTreeNormalNC = { fg = c.white0, bg = c.black },
 
 }
