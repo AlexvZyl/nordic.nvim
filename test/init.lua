e87874cd50e99f6319f693ad197e294e4c4ce3cb
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-25 18:01:16 +0200

    CI: Move to single command

--- test/init.lua
+++ test/init.lua
@@ -3,7 +3,6 @@ local ensure_packer = function()
 	local fn = vim.fn
 	local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 	if fn.empty(fn.glob(install_path)) > 0 then
-        print('Cloning...')
 		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
 		vim.cmd [[packadd packer.nvim]]
 		return true
@@ -24,7 +23,6 @@ return require('packer').startup(function(use)
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
-        print('Bootstrapping...')
 		require('packer').sync()
 	end
 end)
