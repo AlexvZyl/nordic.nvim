b5677c971c90a32ac62e078e1c35436651727f85
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-25 18:10:46 +0200

    CI: Manually install packer

--- test/init.lua
+++ test/init.lua
@@ -1,28 +1,10 @@
--- Bootstrap packer.
-local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-		vim.cmd [[packadd packer.nvim]]
-		return true
-	end
-	return false
-end
+return require('packer').startup( function(use)
 
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
-	use({
+	use {
 		'AlexvZyl/nordic.nvim',
 		-- branch = '',
-	})
+	}
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if packer_bootstrap then
-		require('packer').sync()
-	end
 end)
