7de1f73338f9e028a2db7a632adce2b3e3074dfe
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-25 17:57:18 +0200

    CI: Print bootstrap status

--- test/init.lua
+++ test/init.lua
@@ -3,6 +3,7 @@ local ensure_packer = function()
 	local fn = vim.fn
 	local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 	if fn.empty(fn.glob(install_path)) > 0 then
+        print('Cloning...')
 		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
 		vim.cmd [[packadd packer.nvim]]
 		return true
@@ -23,6 +24,7 @@ return require('packer').startup(function(use)
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
+        print('Bootstrapping...')
 		require('packer').sync()
 	end
 end)
