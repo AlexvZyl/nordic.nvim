a683ccd497ed4013a048575f89929934ac8fb60b
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-01-25 17:05:11 +0200

    CI: Try to force clone

--- test/init.lua
+++ test/init.lua
@@ -2,14 +2,12 @@
 local ensure_packer = function()
 	local fn = vim.fn
 	local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
-		vim.cmd [[packadd packer.nvim]]
-		return true
-	end
-	return false
+	fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+	vim.cmd [[packadd packer.nvim]]
+	return true
 end
-ensure_packer()
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
@@ -21,5 +19,7 @@ return require('packer').startup(function(use)
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
-	require('packer').sync()
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
