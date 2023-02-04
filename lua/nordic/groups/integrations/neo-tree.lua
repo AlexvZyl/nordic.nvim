f7b22a79f6fdb37b8c914e5cd81965941ce4111a
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-02-04 19:24:44 +0200

    Feat: Link neo-tree to nvim-tree

--- lua/nordic/groups/integrations/neo-tree.lua
+++ lua/nordic/groups/integrations/neo-tree.lua
@@ -1,19 +1,18 @@
 local c = require 'nordic.colors'
-local o = require('nordic.config').options
 
 return {
-    NeoTreeCursorLine = { bg = c.bg_selected, fg = c.fg_selected, bold = true },
-    NeoTreeDirectoryIcon = { fg = c.yellow.dim },
-    NeoTreeRootName = { fg = c.yellow.base, bold = true },
-    NeoTreeFileName = { fg = c.white0, bg = o.transparent_bg and c.none or c.black },
-    NeoTreeFileIcon = { fg = c.blue1 },
-    NeoTreeFileNameOpened = { fg = c.blue1 },
-    NeoTreeIndentMarker = { fg = c.grey2 },
+    NeoTreeCursorLine = { link = 'NvimTreeCursorLine' },
+    NeoTreeDirectoryIcon = { link = 'NvimTreeFolderIcon' },
+    NeoTreeRootName = { link = 'NvimTreeRootFolder' },
+    NeoTreeFileName = { link = 'NvimTreeNormal' },
+    NeoTreeFileIcon = { fg = c.blue2 },
+    NeoTreeFileNameOpened = { fg = c.fg },
+    NeoTreeIndentMarker = { link = 'NvimTreeIndentMarker' },
     NeoTreeGitAdded = { fg = c.git.add },
     NeoTreeGitConflict = { fg = c.magenta.bright },
     NeoTreeGitModified = { fg = c.git.change },
     NeoTreeGitUntracked = { fg = c.fg_sidebar },
-    NeoTreeNormal = { fg = c.white0, bg = o.transparent_bg and c.none or c.black },
-    NeoTreeNormalNC = { fg = c.white0, bg = o.transparent_bg and c.none or c.black },
-    NeoTreeSymbolicLinkTarget = { fg = c.blue1 },
+    NeoTreeNormal = { link = 'NvimTreeNormal' },
+    NeoTreeNormalNC = { link = 'NvimTreeNormalNC' },
+    NeoTreeSymbolicLinkTarget = { link = 'NvimTreeSymlink' },
 }
