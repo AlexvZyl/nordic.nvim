a012646aa6d25f1cbf07a746e1bf0d2d7cf10ab3
Alexander van Zyl <calexandervanzyl@gmail.com>
2023-02-06 10:45:11 +0200

    Feat(leap): Backdrop option and better colors (#42)
    * Docs: Update readme
    
    * Docs: Add note about TE
    
    * Feat: Better defaults
    
    * Feat: Lualine maturing
    
    * Fix: Better naming
    
    * Feat: Initial noice support
    
    * Feat: fidget.nvim support
    
    * Feat: Improve existing plugins
    
    * Feat: Start working on reduced blue theme
    
    * Feat: Theme maturing
    
    * Feat: Cleanup and improve nvim-tree
    
    * Feat: Link neo-tree to nvim-tree
    
    * Option to slightly change colors
    
    * Feat: Theme maturing
    
    * Fix: #39
    
    * Docs: Note on WIP
    
    * Fix: Add title hl
    
    * Feat(leap): Add backdrop option and improve colors
    
    * CI(tests): Test new options

--- lua/nordic/tests/options.lua
+++ lua/nordic/tests/options.lua
@@ -6,10 +6,12 @@ local load = require('nordic').load
 load(config)
 
 config.telescope.style = 'classic'
+config.noice.style = 'classic'
 config.bold_keywords = false
 config.italic_comments = false
 config.transparent_bg = true
 config.cursorline.theme = 'light'
 config.cursorline.bold = true
+config.reduced_blue = false
 
 load(config)
