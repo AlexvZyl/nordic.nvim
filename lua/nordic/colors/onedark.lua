d87edba4900722e9223c90afda0acbc5ef507d18
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-02-11 02:33:53 +0200

    Feat: Outline for onedark

---
+++ lua/nordic/colors/onedark.lua
@@ -0,0 +1,68 @@
+-- The ondark palette from NvChad: 
+-- https://github.com/NvChad/base46/blob/master/lua/base46/themes/onedark.lua.
+
+local o = require 'nordic.config' .options
+
+local palette = {
+
+    none = 'NONE',
+
+    black = '',
+
+    -- Grays,
+    gray0 = '',
+    gray1 = '',
+    gray2 = '',
+    gray3 = '',
+    gray4 = '',
+    gray5 = '',
+
+    -- White.
+    white0 = '',
+    white1 = '',
+    white2 = '',
+    white3 = '',
+
+    -- Blues.
+    blue0 = '',
+    blue1 = '',
+    blue2 = '',
+    blue = {
+        bright = '',
+        dim = '',
+    },
+
+    cyan = {
+        base = '',
+        bright = '',
+        dim = '',
+    },
+    red = {
+        base = '',
+        bright = '',
+        dim = '',
+    },
+    orange = {
+        base = '',
+        bright = '',
+        dim = '',
+    },
+    yellow = {
+        base = '',
+        bright = '',
+        dim = '',
+    },
+    green = {
+        base = '',
+        bright = '',
+        dim = '',
+    },
+    magenta = {
+        base = '',
+        bright = '',
+        dim = '',
+    }
+
+}
+
+return palette
