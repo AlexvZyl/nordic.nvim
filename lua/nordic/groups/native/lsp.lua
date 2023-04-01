3d08d425a473a7df5764cdd81e686260e219c87c
Alexander van Zyl <calexandervanzyl@gmail.com>
2023-04-01 19:11:33 +0200

    Feat: Add groups used in tokyonight

--- lua/nordic/groups/native/lsp.lua
+++ lua/nordic/groups/native/lsp.lua
@@ -61,4 +61,21 @@ return {
     LspTypeParameter = { link = '@type' },
     LspVariable = { link = '@variable' },
 
+    -- LSP Semantic Token Groups
+    ["@lsp.type.comment"] = { link = "@comment" },
+    ["@lsp.type.enum"] = { link = "@type" },
+    ["@lsp.type.interface"] = { link = "Identifier" },
+    ["@lsp.type.keyword"] = { link = "@keyword" },
+    ["@lsp.type.namespace"] = { link = "@namespace" },
+    ["@lsp.type.parameter"] = { link = "@parameter" },
+    ["@lsp.type.property"] = { link = "@property" },
+    ["@lsp.type.variable"] = {}, -- use treesitter styles for regular variables
+    ["@lsp.typemod.method.defaultLibrary"] = { link = "@function.builtin" },
+    ["@lsp.typemod.function.defaultLibrary"] = { link = "@function.builtin" },
+    ["@lsp.typemod.operator.injected"] = { link = "@operator" },
+    ["@lsp.typemod.string.injected"] = { link = "@string" },
+    ["@lsp.typemod.variable.defaultLibrary"] = { link = "@variable.builtin" },
+    ["@lsp.typemod.variable.injected"] = { link = "@variable" },
+    -- ["@lsp.typemod.variable.globalScope"] (global variables)
+
 }
