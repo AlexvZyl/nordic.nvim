3247a0e280f8f8320aca5b38de16c4c56c2de772
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-04-23 21:45:16 +0200

    Feat: Initial nvim-cmp support

--- lua/nordic/groups/integrations/nvim-cmp.lua
+++ lua/nordic/groups/integrations/nvim-cmp.lua
@@ -2,13 +2,41 @@ local c = require 'nordic.colors'
 
 return {
 
-    BorderBG = {
-        fg = c.white0,
-    },
+    CmpItemAbbrDeprecated = { fg = c.grey4},
+    CmpItemAbbrMatch = { fg = c.yellow.bright, bold = true },
+    CmpItemAbbrMatchFuzzy = { fg = c.yellow.bright, bold = true },
 
-    PmenuSel = {
-        bg = c.yellow,
-        fg = c.black,
-        bold = true,
-    },
+    CmpItemKindField = { link = '@field' },
+    CmpItemKindProperty = { link = '@proprty' },
+    CmpItemKindEvent = { link = 'Type' },
+
+    CmpItemKindText = { fg = c.grey4 },
+    CmpItemKindEnum = { link = 'Type' },
+    CmpItemKindKeyword = { link = 'Keyword' },
+
+    CmpItemKindConstant = { link  = 'Constant' },
+    CmpItemKindConstructor = { link = 'Function'},
+    CmpItemKindReference = { link = 'Variable' },
+
+    CmpItemKindFunction = { link = 'Function' },
+    CmpItemKindStruct = { link = 'Type' },
+    CmpItemKindClass = { link = 'Type' },
+    CmpItemKindModule = { link = 'Macro' },
+    CmpItemKindOperator = { link = 'Operator' },
+
+    CmpItemKindVariable = { link = '@variable' },
+    CmpItemKindFile = { fg = c.blue1 },
+
+    CmpItemKindUnit = { link = 'Constant' },
+    CmpItemKindSnippet = { },
+    CmpItemKindFolder = { fg = c.yellow.dark },
+
+    CmpItemKindMethod = { link = 'Function' },
+    CmpItemKindValue = { link = 'Constant' },
+    CmpItemKindEnumMember = { link = 'Type' },
+
+    CmpItemKindInterface = { link = 'Type' },
+    CmpItemKindColor = { link = 'Constant' },
+    CmpItemKindTypeParameter = { link = 'Type' },
+
 }
