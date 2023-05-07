25b71f890508af7d3a2226dc1562e136f87d9801
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-05-07 13:37:41 +0200

    Feat: Improve barbar colors

--- lua/nordic/groups/integrations/barbar.lua
+++ lua/nordic/groups/integrations/barbar.lua
@@ -1,68 +1,32 @@
 local c = require 'nordic.colors'
+local blend = require 'nordic.utils'.blend
+
+local barbar_bg = blend(c.gray0, c.black, 0.25)
 
 return {
 
-    BufferCurrent = {
-        fg = c.white0,
-        bg = c.gray0,
-        bold = false,
-    },
+    -- Normals.
+    BufferCurrent = { fg = c.white0, bg = c.gray0, bold = false },
+    BufferVisible = { fg = c.white0, bg = barbar_bg },
+    BufferInactive = { fg = c.gray3, bg = barbar_bg },
 
-    BufferCurrentSign = {
-        fg = c.orange.bright,
-        bg = c.gray0,
-        bold = true,
-    },
+    -- Signs.
+    BufferVisibleSign = { fg = c.black, bg = c.black, bold = true },
+    BufferCurrentSign = { fg = c.orange.bright, bg = c.gray0, bold = true },
+    BufferInactiveSign = { bg = c.black, fg = c.black },
 
-    BufferCurrentMod = {
-        fg = c.white0,
-        bg = c.gray0,
-        bold = true,
-    },
+    -- Mods.
+    BufferCurrentMod = { fg = c.white0, bg = c.gray0, bold = true },
+    BufferVisibleMod = { fg = c.white0, bg = c.black },
 
-    BufferVisible = {
-        fg = c.white0,
-        bg = c.black,
-    },
+    -- Diagnostics.
+    BufferVisibleError = { fg = c.red.bright, bg = c.black },
 
-    BufferVisibleSign = {
-        fg = c.black,
-        bg = c.black,
-        bold = true,
-    },
-
-    BufferVisibleMod = {
-        fg = c.white0,
-        bg = c.black,
-    },
-
-    BufferVisibleError = {
-        fg = c.red.bright,
-        bg = c.black,
-    },
-
-    BufferInactive = {
-        fg = c.gray4,
-        bg = c.black,
-    },
-
-    BufferCurrentTarget = {
-        fg = c.red.bright,
-        bold = true,
-    },
-    BufferVisibleTarget = {
-        fg = c.red.bright,
-        bold = true
-    },
-    BufferInactiveTarget = {
-        bg = c.bg_dark,
-        fg = c.red.bright,
-        bold = true
-    },
-    BufferAlternateTarget = {
-        fg = c.red.bright,
-        bold = true
-    }
+    -- Targets.
+    BufferCurrentTarget = { fg = c.red.bright, bold = true },
+    BufferVisibleTarget = { fg = c.red.bright, bold = true },
+    BufferInactiveTarget = { bg = c.bg_dark, fg = c.red.bright, bold = true },
+    BufferAlternateTarget = { fg = c.red.bright, bold = true }
 
     -- BufferCurrentIndex = { bg = C.surface1, fg = C.blue },
     -- BufferCurrentTarget = { bg = C.surface1, fg = C.red },
@@ -70,7 +34,6 @@ return {
     -- BufferVisibleTarget = { bg = C.mantle, fg = C.red },
     -- BufferInactive = { bg = C.mantle, fg = C.overlay0 },
     -- BufferInactiveIndex = { bg = C.mantle, fg = C.overlay0 },
-    -- BufferInactiveSign = { bg = C.mantle, fg = C.blue },
     -- BufferInactiveTarget = { bg = C.mantle, fg = C.red },
     -- BufferTabpages = { bg = C.mantle, fg = C.none },
     -- BufferTabpage = { bg = C.mantle, fg = C.blue },
