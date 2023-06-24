862abe9f619eccd4c1c8fe44d2f06da670abbcd4
Alexander van Zyl <alexandervanzyl@protonmail.com>
2023-06-24 10:31:29 +0200

    Lspsaga: Tweak definition colors

--- lua/nordic/groups/integrations/lspsaga.lua
+++ lua/nordic/groups/integrations/lspsaga.lua
@@ -5,8 +5,8 @@ return {
     SagaBorder = { fg = c.border_float, bg = c.bg_dark },
     SagaNormal = { bg = c.bg_dark },
 
-    DefinitionBorder = { bg = c.bg_dark, fg = c.gray1 },
-    DefinitionNormal = { bg = c.bg_dark },
+    DefinitionBorder = { bg = c.bg, fg = c.black },
+    DefinitionNormal = { link = 'Normal' },
 
     DiagnosticPos = { fg = c.fg },
     DiagnosticWord = { fg = c.fg },
