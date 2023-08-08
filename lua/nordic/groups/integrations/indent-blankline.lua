9e00e2d20eabd4fefbb7138a60d75579a457d39d
AlexvZyl <alexandervanzyl@protonmail.com>
2023-08-08 13:07:20 +0200

    Colors: Brighten indent blankline

--- lua/nordic/groups/integrations/indent-blankline.lua
+++ lua/nordic/groups/integrations/indent-blankline.lua
@@ -2,6 +2,6 @@ local C = require 'nordic.colors'
 
 return {
     IndentBlanklineChar = { fg = C.gray1 },
-    IndentBlanklineContextChar = { fg = C.gray1 },
+    IndentBlanklineContextChar = { fg = C.gray2 },
     IndentBlanklineContextStart = { link = 'IndentBlanklineContextChar' },
 }
