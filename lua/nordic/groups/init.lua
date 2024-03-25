fce8d79563aacd7558ebe54bd1eac9d832284d6c
5-pebbles <5-pebble@protonmail.com>
2024-03-25 20:30:57 +0000

    feat(deprecated): re add override option with deprecated warning;

--- lua/nordic/groups/init.lua
+++ lua/nordic/groups/init.lua
@@ -3,12 +3,21 @@ local merge = require('nordic.utils').merge
 local M = {}
 
 function M.get_groups()
-    local O = require('nordic.config').options
-
     local groups = merge(
         require('nordic.groups.native').get_groups(),
         require('nordic.groups.integrations').get_groups()
     )
+
+    local O = require('nordic.config').options
+
+    if O.override ~= nil then
+        vim.api.nvim_echo(
+        { { 'nordic.nvim override config option is now depricated, please use on_highlight', 'WarningMsg' } }, true, {})
+        groups = merge(
+            groups,
+            O.override
+        )
+    end
 
     return O.on_highlight(groups, require('nordic.colors'))
 end
