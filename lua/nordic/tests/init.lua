cda0836e47ccf903e08c3d90ef348df44212c6b9
5-pebbles <5-pebble@protonmail.com>
2024-06-12 14:12:04 -0400

    feat(tests): add assert_eq to test module;

--- lua/nordic/tests/init.lua
+++ lua/nordic/tests/init.lua
@@ -1,1 +1,21 @@
-require("nordic.tests.options")
+M = {}
+
+function M.run_tests()
+    require('nordic.tests.options')
+    require('nordic.tests.utils')
+end
+
+function M.assert_eq(left, right, message)
+    if left ~= right then
+        local info = debug.getinfo(2)
+        local file_name = info.short_src
+        local line_number = info.currentline
+        print('\nassertion `left == right` failed' ..
+            ((message and ': ' .. message) or '') ..
+            '\n  left: ' ..
+            tostring(left) ..
+            '\n right: ' .. tostring(right) .. '\nat ' .. tostring(file_name) .. ':' .. tostring(line_number))
+    end
+end
+
+return M
---
+++ lua/nordic/tests/utils.lua
@@ -0,0 +1,7 @@
+local assert_eq = require("nordic.tests").assert_eq
+
+-- TODO remove after you are done testing
+assert_eq(4, 6)
+assert_eq(4, 5)
+assert_eq(4, 4)
+
