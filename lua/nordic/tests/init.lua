c117dae4ac667df454cf0f635a1b8eae4e2c8ba4
5-pebbles <5-pebble@protonmail.com>
2024-06-14 16:38:39 -0400

    feat(tests): assert_eq works on tables now;

--- lua/nordic/tests/init.lua
+++ lua/nordic/tests/init.lua
@@ -1,12 +1,22 @@
 M = {}
 
-function M.run_tests()
-    require('nordic.tests.options')
-    require('nordic.tests.utils')
-end
-
 function M.assert_eq(left, right, message)
-    if left ~= right then
+    local function recursive_eq(value1, value2)
+        if type(value1) ~= "table" or type(value2) ~= "table" then
+            return value1 == value2
+        end
+
+        -- I think this can be done faster but its just for tests...
+        for k, v in pairs(value1) do
+            if not recursive_eq(v, value2[k]) then return false end
+        end
+        for k, v in pairs(value2) do
+            if not recursive_eq(v, value1[k]) then return false end
+        end
+        return true
+    end
+
+    if not recursive_eq(left, right) then
         local info = debug.getinfo(2)
         local file_name = info.short_src
         local line_number = info.currentline
@@ -16,6 +26,12 @@ function M.assert_eq(left, right, message)
             tostring(left) ..
             '\n right: ' .. tostring(right) .. '\nat ' .. tostring(file_name) .. ':' .. tostring(line_number))
     end
+end
+
+function M.run_tests()
+    require('nordic.tests.options')
+    require('nordic.tests.utils')
+
 end
 
 return M
