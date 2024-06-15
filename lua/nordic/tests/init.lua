a109c948c6d236f1ecfe0f69ac956bc7f4811aa4
5-pebbles <5-pebble@protonmail.com>
2024-06-15 13:13:58 -0400

    feat(tests): readable table asserts;

--- lua/nordic/tests/init.lua
+++ lua/nordic/tests/init.lua
@@ -1,8 +1,23 @@
+local U = require('nordic.utils')
+
 M = {}
+
+local function pretty_string(value)
+    if not U.is_table(value) then return tostring(value) end
+    local output = '{'
+    for k, v in pairs(value) do
+        output = output .. '' .. tostring(k) .. ': ' .. pretty_string(v) .. ', '
+    end
+    if #output == 1 then
+        return output .. '}'
+    else
+        return output:sub(1, -3) .. '}'
+    end
+end
 
 function M.assert_eq(left, right, message)
     local function recursive_eq(value1, value2)
-        if type(value1) ~= "table" or type(value2) ~= "table" then
+        if not U.is_table(value1) or not U.is_table(value2) then
             return value1 == value2
         end
 
@@ -23,8 +38,8 @@ function M.assert_eq(left, right, message)
         print('\nassertion `left == right` failed' ..
             ((message and ': ' .. message) or '') ..
             '\n  left: ' ..
-            tostring(left) ..
-            '\n right: ' .. tostring(right) .. '\nat ' .. tostring(file_name) .. ':' .. tostring(line_number))
+            pretty_string(left) ..
+            '\n right: ' .. pretty_string(right) .. '\nat ' .. tostring(file_name) .. ':' .. tostring(line_number))
     end
 end
 
