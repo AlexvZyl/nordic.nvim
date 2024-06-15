f5ff09c039d87865fb8e4ae4fdafe07358b7a37a
5-pebbles <5-pebble@protonmail.com>
2024-06-15 09:59:55 -0400

    fix(tests): fix bad module name in error messages;

--- lua/nordic/tests/utils.lua
+++ lua/nordic/tests/utils.lua
@@ -10,16 +10,16 @@ assert_eq(U.is_none('none'), true, 'U.is_none("none") should return true')
 assert_eq(U.is_none('nil'), false, 'U.is_none("nil") should return false')
 
 -- merge
-assert_eq(U.merge({}, {}), {}, 'M.merge({}, {}) should return an empty table')
+assert_eq(U.merge({}, {}), {}, 'U.merge({}, {}) should return an empty table')
 
-assert_eq(U.merge(nil, nil), {}, 'M.merge(nil, nil) should return an empty table')
+assert_eq(U.merge(nil, nil), {}, 'U.merge(nil, nil) should return an empty table')
 
-assert_eq(U.merge(nil, {a = 1}), {a = 1}, 'M.merge(nil, {a = 1}) should return {a = 1}')
-assert_eq(U.merge({a = 1}, nil), {a = 1}, 'M.merge({a = 1}, nil) should return {a = 1}')
+assert_eq(U.merge(nil, {a = 1}), {a = 1}, 'U.merge(nil, {a = 1}) should return {a = 1}')
+assert_eq(U.merge({a = 1}, nil), {a = 1}, 'U.merge({a = 1}, nil) should return {a = 1}')
 
-assert_eq(U.merge({a = 1, b = 3}, {b = 2, c = 4}), {a = 1, b = 2, c = 4}, 'M.merge({a = 1, b = 3}, {b = 2, c = 4}) should return {a = 1, b = 2, c = 4}')
+assert_eq(U.merge({a = 1, b = 3}, {b = 2, c = 4}), {a = 1, b = 2, c = 4}, 'U.merge({a = 1, b = 3}, {b = 2, c = 4}) should return {a = 1, b = 2, c = 4}')
 
 -- hex_to_rgb
-assert_eq({ U.hex_to_rgb('#191D24') }, {25, 29, 36}, 'M.hex_to_rgb("#191D24") should return 25, 29, 36')
+assert_eq({ U.hex_to_rgb('#191D24') }, {25, 29, 36}, 'U.hex_to_rgb("#191D24") should return 25, 29, 36')
 
 
