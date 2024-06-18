a9556c57e0ae45422cff48ad41785c1ec950131f
Avi Herman <aviherman5@gmail.com>
2024-06-17 23:10:58 -0400

    added support for rainbow-delimeters.nvim

--- lua/nordic/groups/integrations.lua
+++ lua/nordic/groups/integrations.lua
@@ -523,6 +523,13 @@ function M.get_groups()
     G.WhichKeySeperator = {}
     G.WhichKeyValue = {}
 
+    -- Rainbow delimiters
+    G.RainbowDelimiterOrange = { fg = C.orange.base }
+    G.RainbowDelimiterYellow = { fg = C.yellow.bright }
+    G.RainbowDelimiterBlue = { fg = C.blue2 }
+    G.RainbowDelimiterRed = { fg = C.red.bright }
+    G.RainbowDelimiterGreen = { fg = C.green.bright}
+
     return G
 end
 
