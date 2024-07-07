61f4b47d4445b367fe8734169cbb3549c648af5a
5-pebbles <5-pebble@protonmail.com>
2024-07-07 13:37:18 -0400

    feat(tests): make highlight return hexed colors;

--- lua/nordic/tests/options.lua
+++ lua/nordic/tests/options.lua
@@ -3,20 +3,28 @@ local load = require('nordic').load
 local U = require('nordic.utils')
 
 local function highlight(group)
-    return vim.api.nvim_get_hl(0, { name = group, create = false })
-end
+  local function hexify(value)
+    if type(value) == 'number' then
+      return string.format('#%X', value)
+    elseif type(value) == 'table' then
+      return vim.tbl_map(hexify, value)
+    end
+    return value
+  end
 
+  return hexify(vim.api.nvim_get_hl(0, { name = group, create = false }))
+end
 
 load({})
 
 -- bold_keywords
 assert_eq(highlight('Keyword').bold, nil, 'highlight `Keyword` should not be bold by default')
-load({bold_keywords = true})
+load({ bold_keywords = true })
 assert_eq(highlight('Keyword').bold, true, 'highlight `Keyword` should be bold if `bold_keywords` is true')
-load({bold_keywords = false})
+load({ bold_keywords = false })
 
 -- italic_comments
 assert_eq(highlight('Comment').italic, true, 'highlight `Comments` should be italic by default')
-load({italic_comments = false})
+load({ italic_comments = false })
 assert_eq(highlight('Comment').italic, nil, 'highlight `Comments` should not be italic if `italic_comments` is false')
-load({italic_comments = true})
+load({ italic_comments = true })
