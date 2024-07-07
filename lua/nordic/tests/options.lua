a5a74f38918b89220ec8615eb6b985346083d2fd
5-pebbles <5-pebble@protonmail.com>
2024-07-07 12:01:04 -0400

    feat(tests): add state tests for bold_keywords;

--- lua/nordic/tests/options.lua
+++ lua/nordic/tests/options.lua
@@ -9,6 +9,12 @@ end
 
 load({})
 
+-- bold_keywords
+assert_eq(highlight('Keyword').bold, nil, 'highlight `Keyword` should not be bold by default')
+load({bold_keywords = true})
+assert_eq(highlight('Keyword').bold, true, 'highlight `Keyword` should be bold if `bold_keywords` is true')
+load({bold_keywords = false})
+
 -- italic_comments
 assert_eq(highlight('Comment').italic, true, 'highlight `Comments` should be italic by default')
 load({italic_comments = false})
