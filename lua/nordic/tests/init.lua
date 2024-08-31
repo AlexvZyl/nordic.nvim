4bcea922cbd19180f7f4227f4804988f32020178
5-pebbles <5-pebble@protonmail.com>
2024-08-31 18:46:56 -0400

    fix(tests): replace pretty_string with vim.inspect();

--- lua/nordic/tests/init.lua
+++ lua/nordic/tests/init.lua
@@ -2,19 +2,6 @@ local U = require('nordic.utils')
 
 M = {}
 
-local function pretty_string(value)
-    if not U.is_table(value) then return tostring(value) end
-    local output = '{'
-    for k, v in pairs(value) do
-        output = output .. '' .. tostring(k) .. ': ' .. pretty_string(v) .. ', '
-    end
-    if #output == 1 then
-        return output .. '}'
-    else
-        return output:sub(1, -3) .. '}'
-    end
-end
-
 function M.assert_eq(left, right, message)
     local function recursive_eq(value1, value2)
         if not U.is_table(value1) or not U.is_table(value2) then
@@ -38,8 +25,8 @@ function M.assert_eq(left, right, message)
         print('\nassertion `left == right` failed' ..
             ((message and ': ' .. message) or '') ..
             '\n  left: ' ..
-            pretty_string(left) ..
-            '\n right: ' .. pretty_string(right) .. '\nat ' .. tostring(file_name) .. ':' .. tostring(line_number))
+            vim.inspect(left) ..
+            '\n right: ' .. vim.inspect(right) .. '\nat ' .. tostring(file_name) .. ':' .. tostring(line_number))
     end
 end
 
