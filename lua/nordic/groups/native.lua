ca1813e7fc11373416af841261d0fe7b82fab419
jorgebef <jorge.befan@gmail.com>
2025-03-03 20:25:58 +0100

    Remove mentions to bg_cursorline in favor of bg_visual

--- lua/nordic/groups/native.lua
+++ lua/nordic/groups/native.lua
@@ -141,13 +141,13 @@ function M.get_groups()
     G.TreeWinSeparator = { link = 'WinSeparator' }
 
     G.Comment = { fg = C.comment, italic = O.italic_comments } -- any comment
-    G.ColorColumn = { bg = C.bg_cursorline } -- used for the columns set with 'colorcolumn'
+    G.ColorColumn = { bg = C.bg_visual } -- used for the columns set with 'colorcolumn'
     G.Conceal = { fg = C.gray3 } -- placeholder characters substituted for concealed text (see 'conceallevel')
     G.Cursor = { fg = C.black0, bg = C.fg } -- character under the cursor
     G.lCursor = { fg = C.black0 } -- the character under the cursor when |language-mapping| is used (see 'guicursor')
     G.CursorIM = { fg = C.black0 } -- like Cursor, but used when in IME mode |CursorIM|
-    G.CursorColumn = { bg = C.bg_cursorline, bold = O.cursorline.bold } -- Screen-column at the cursor, when 'cursorcolumn' is set.
-    G.CursorLine = { bg = C.bg_cursorline, bold = O.cursorline.bold } -- Screen-line at the cursor, when 'cursorline' is set.  Low-priority if foreground (ctermfg OR guifg) is not set.
+    G.CursorColumn = { bg = C.bg_visual, bold = O.cursorline.bold } -- Screen-column at the cursor, when 'cursorcolumn' is set.
+    G.CursorLine = { bg = C.bg_visual, bold = O.cursorline.bold } -- Screen-line at the cursor, when 'cursorline' is set.  Low-priority if foreground (ctermfg OR guifg) is not set.
     G.CursorLineNr = { fg = C.gray5, bold = O.cursorline.bold_number } -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
     G.CursorLineSign = {}
     G.Directory = { fg = C.blue1 } -- directory names (and other special names in listings)
@@ -180,9 +180,9 @@ function M.get_groups()
     G.PmenuSbar = { bg = C.gray2 } -- Popup menu: scrollbar.
     G.PmenuThumb = { bg = C.gray2, fg = C.gray2 } -- Popup menu: Thumb of the scrollbar.
     G.Question = { fg = C.info } -- |hit-enter| prompt and yes/no questions
-    G.QuickFixLine = { bg = C.bg_cursorline, bold = true } -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
-    G.Search = { bg = C.bg_cursorline, fg = C.yellow.bright, bold = true, underline = true } -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
-    G.IncSearch = { bg = C.yellow.base, fg = C.bg_cursorline, bold = true } -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+    G.QuickFixLine = { bg = C.bg_visual, bold = true } -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
+    G.Search = { bg = C.bg_visual, fg = C.yellow.bright, bold = true, underline = true } -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
+    G.IncSearch = { bg = C.yellow.base, fg = C.bg_visual, bold = true } -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
     G.CurSearch = { link = 'IncSearch' }
     G.SpecialKey = { fg = C.gray5 } -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace|
     G.SpellBad = { sp = C.error, undercurl = true } -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
@@ -199,7 +199,7 @@ function M.get_groups()
     G.VisualNOS = { bg = C.bg_visual } -- Visual mode selection when vim is "Not Owning the Selection".
     G.WarningMsg = { fg = C.warning } -- warning messages
     G.Whitespace = { fg = C.gray4 } -- "nbsp", "space", "tab" and "trail" in 'listchars'
-    G.WildMenu = { bg = C.bg_cursorline } -- current match in 'wildmenu' completion
+    G.WildMenu = { bg = C.bg_visual } -- current match in 'wildmenu' completion
     G.WinBar = { bg = C.bg_dark, fg = C.gray5 }
     G.WinBarNC = { bg = C.bg_dark, fg = C.gray4 }
 
