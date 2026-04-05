9fb64042f2d1e8a28586cee45a75f7a208bed3ca
Jacob R. <jacobrambarran@proton.me>
2026-04-05 07:34:19 -0400

    feat(integrations): add highlights for mini.jump2d (#190)
    Co-authored-by: Jacob R. <jbrm@noreply.codeberg.org>

--- lua/nordic/groups/integrations/mini.lua
+++ lua/nordic/groups/integrations/mini.lua
@@ -2,9 +2,10 @@ local M = {}
 
 function M.get()
     local LC = require('lualine.themes.nordic')
+    local C = require('nordic.colors')
 
     local G = {}
-
+    -- Mini.StatusLine
     G.MiniStatuslineModeNormal = { bg = LC.normal.a.bg, fg = LC.normal.a.fg, bold = LC.normal.a.gui == 'bold' }
     G.MiniStatuslineModeInsert = { bg = LC.insert.a.bg, fg = LC.insert.a.fg, bold = LC.insert.a.gui == 'bold' }
     G.MiniStatuslineModeVisual = { bg = LC.visual.a.bg, fg = LC.visual.a.fg, bold = LC.visual.a.gui == 'bold' }
@@ -15,6 +16,12 @@ function M.get()
     G.MiniStatuslineFilename = { bg = LC.normal.c.bg, fg = LC.normal.c.fg }
     G.MiniStatuslineFileinfo = { bg = LC.normal.b.bg, fg = LC.normal.b.fg }
     G.MiniStatuslineInactive = { bg = LC.inactive.a.bg, fg = LC.inactive.a.fg, bold = LC.inactive.a.gui == 'bold' }
+
+    -- Mini.Jump2d
+    G.MiniJump2dSpot = { bg = C.gray2, fg = C.cyan.dim }
+    G.MiniJump2dSpotAhead = { bg = C.gray2, fg = C.orange.dim }
+    G.MiniJump2dSpotUnique = { bg = C.gray2, fg = C.yellow.dim }
+    G.MiniJump2dDim = { link = 'Comment' }
 
     return G
 end
